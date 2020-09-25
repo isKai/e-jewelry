e438ccf06323ce253a1375dbbbf770f3f72cbb6f
isKai <kaichen@jmu.edu.cn>
2020-09-25 14:56:55 +0800

    Initial commit

--- contracts/ConvertLib.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
-
-library ConvertLib {
-    function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount) {
-        return amount * conversionRate;
-    }
-}
